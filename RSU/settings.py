3cd51eff7061c6c204a7b39f55875fe71abc91aa
vikaas-cyper <avikaas17@gmail.com>
2023-10-30 14:10:40 +0530

    Added rsu calculator with migration

--- RSU/settings.py
+++ RSU/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rsu_calculator'
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,8 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages'
+                
             ],
         },
     },
