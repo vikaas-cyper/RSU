7467b618510b5ba6c179fde547d22409c24082d0
vikaas-cyper <avikaas17@gmail.com>
2023-12-16 11:33:45 +0530

    New Start

--- EquityElevation/wsgi.py
+++ EquityElevation/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for RSU project.
+WSGI config for EquityElevation project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RSU.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EquityElevation.settings')
 
 application = get_wsgi_application()
--- rsu_calculator/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RsuCalculatorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rsu_calculator'
