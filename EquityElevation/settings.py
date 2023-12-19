7f4e641f784d6a5e33596dd8315470782303b752
vikaas-cyper <avikaas17@gmail.com>
2023-12-19 20:35:06 +0530

    Model Initialization

--- EquityElevation/settings.py
+++ EquityElevation/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'EmployeeDetails'
+    'EmployeeDetails',
+    'django_countries'
 ]
 
 MIDDLEWARE = [
